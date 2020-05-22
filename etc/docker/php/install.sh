8f48f0cc369a8aa0af631d7e19fa2cc70679ad8d
Yaroslav Chupikov <yaroslav@mirasoltek.com>
2020-05-22 13:16:45 +0300

    Added software for image manipulation. WARNING! Only 'pngout' workable. Refs #9

--- etc/docker/php/install.sh
+++ etc/docker/php/install.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 # Install dependencies
 
+declare -A files
+files=(
+    ["zopflipng"]="https://github.com/imagemin/zopflipng-bin/raw/master/vendor/linux/zopflipng"
+    ["pngcrush"]="https://github.com/imagemin/pngcrush-bin/raw/master/vendor/linux/pngcrush"
+    ["jpegoptim"]="https://github.com/imagemin/jpegoptim-bin/raw/master/vendor/linux/jpegoptim"
+    ["pngout"]="https://github.com/imagemin/pngout-bin/raw/master/vendor/linux/x64/pngout"
+    ["advpng"]="https://github.com/imagemin/advpng-bin/raw/master/vendor/linux/advpng"
+    ["cjpeg"]="https://github.com/imagemin/mozjpeg-bin/raw/master/vendor/linux/cjpeg"
+)
+
 
 ## Install Composer
+
+echo "Install Composer..."
 
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 wget -q https://getcomposer.org/installer -O composer-setup.php
@@ -19,4 +31,14 @@ php composer-setup.php --quiet
 RESULT=$?
 rm composer-setup.php
 mv composer.phar /usr/local/bin/composer
-exit $RESULT
+
+
+## Install listed dependencies
+
+for file in "${!files[@]}"; do
+  url=${files[$file]}
+  filename="/usr/local/bin/${file}"
+  echo "Install '${filename}'..."
+  wget -q "${url}" -O "${filename}"
+  chmod 0755 "${filename}"
+done
